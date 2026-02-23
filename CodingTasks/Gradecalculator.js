ce38cd83a153c533d632f85efd79d67285d72ae7
Saumitra <ishtripathi7@gmail.com>
2026-02-23 21:59:16 +0530

    feat: Add a grade calculator function to determine grades based on marks.

---
+++ CodingTasks/Gradecalculator.js
@@ -0,0 +1,19 @@
+function gradeCalculator(marks) {
+    if (marks >= 90) {
+        console.log("Grade A");
+    }
+    else if (marks >= 80) {
+        console.log("Grade B");
+    }
+    else if (marks >= 70) {
+        console.log("Grade C");
+    }
+    else if (marks >= 60) {
+        console.log("Grade D");
+    }
+    else {
+        console.log("Grade F");
+    }
+}
+let marks = 76;
+gradeCalculator(marks);
