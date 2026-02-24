602471cb51a155eb6cf233e510b2b6114b51b3b3
Saumitra <ishtripathi7@gmail.com>
2026-02-24 14:28:30 +0530

    exercise_41

---
+++ LearningPlaywrightBatch/chapter_05_Statements/41_IQ.js
@@ -0,0 +1,11 @@
+let a = 10;
+let b = 20;
+let c = 30;
+
+if (a > b && a > c) {
+    console.log("a is the largest number");
+} else if (b > a && b > c) {
+    console.log("b is the largest number");
+} else {
+    console.log("c is the largest number");
+}
