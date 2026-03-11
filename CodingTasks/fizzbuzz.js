175bd542c1e48de495a93624cf25699a1ad6bb98
Saumitra <ishtripathi7@gmail.com>
2026-03-12 01:14:05 +0530

    feat: Initialize Playwright project structure with configuration, example tests, and add various JavaScript learning examples.

--- CodingTasks/fizzbuzz.js
+++ CodingTasks/fizzbuzz.js
@@ -1,12 +1,12 @@
-let a = 1;
+
 for (let i = 1; i <= 100; i++) {
-    if (i % 3 == 0 && i % 5 == 0) {
+    if (i % 3 === 0 && i % 5 === 0) {
         console.log("FizzBuzz");
     }
-    else if (i % 3 == 0) {
+    else if (i % 3 === 0) {
         console.log("Fizz");
     }
-    else if (i % 5 == 0) {
+    else if (i % 5 === 0) {
         console.log("Buzz");
     }
     else {
